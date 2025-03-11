a33ee234c46dcf17a13a90ef9f9123beb298d335
Joanne Lin <y3933y3933@gmail.com>
2025-03-11 20:19:33 +0800

    feat: add list filter

--- command/list.go
+++ command/list.go
@@ -13,15 +13,59 @@ func handlerList(args []string) error {
 	}
 
 	tasks, err := task.LoadTask()
+	if err != nil {
+		return err
+	}
 
 	if len(tasks) == 0 {
 		fmt.Println("No tasks found.")
+		return nil
+	}
+	if len(args) == 0 {
+		printTask(tasks)
+		return nil
 	}
 
-	for _, task := range tasks {
+	status := args[0]
 
+	if isValidStatus(status) {
+		filteredTasks := filterByStatus(tasks, status)
+		printTask(filteredTasks)
+
+	} else {
+		return errors.New("usage: task-cli list <todo | done | in-progress>")
+	}
+
+	return nil
+}
+
+func printTask(tasks []task.Task) {
+
+	for _, task := range tasks {
 		fmt.Printf("%v %s %s\n", task.ID, task.Description, task.Status)
 
 	}
-	return err
+}
+
+func filterByStatus(tasks []task.Task, status string) []task.Task {
+
+	result := []task.Task{}
+	for _, task := range tasks {
+		if task.Status == status {
+			result = append(result, task)
+		}
+	}
+
+	return result
+
+}
+
+func isValidStatus(status string) bool {
+	validStatuses := map[string]bool{
+		"todo":        true,
+		"in-progress": true,
+		"done":        true,
+	}
+	_, exist := validStatuses[status]
+	return exist
 }
